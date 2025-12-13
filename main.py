6adf0dce470ccd138e8e8051fbadd089e58041ee
Conor O'Brien <obriencjj@gmail.com>
2025-12-13 20:13:27 +0000

    Switch to browser authentication
    See [issue](https://github.com/sigma67/ytmusicapi/issues/813) for more details.
    
    Load config.

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 import logging
-import os
+import yaml
+from ytmusicapi import YTMusic
 
-from dotenv import load_dotenv
-from ytmusicapi import OAuthCredentials, YTMusic
+logging.basicConfig(level=logging.DEBUG)
 
-logging.basicConfig(level=logging.INFO)
 
-load_dotenv()
+def load_config():
+    logging.debug("Loading config...")
+    with open("config.yml", "r") as f:
+        return yaml.safe_load(f)
 
-ytmusic = YTMusic(
-    "oauth.json",
-    oauth_credentials=OAuthCredentials(
-        client_id=os.getenv("CLIENT_ID"),
-        client_secret=os.getenv("CLIENT_SECRET"),
-    ),
-)
 
-logging.info(bool(ytmusic))
+if __name__ == "__main__":
+
+    cfg = load_config()
+
+    ytmusic = YTMusic(
+        "browser.json",
+    )
+
+    all_playlists = ytmusic.get_library_playlists()
+
+    print(len(all_playlists))
+
+    # Loop 1: check if we should even proceed.
+    for p in all_playlists:
+        if p["title"] not in cfg["organise"] and p["title"] not in cfg["ignore"]:
+            logging.error(f"There's an errant playlist floating around...")
+            logging.error(f"Playlist: {p['title']}")
+            exit(1)
+
+    # Loop 2: filter the relevant playlists.
+    # Yes, this could be 1 loop. This also isn't NASA.
+    # We're not solving for performance.
+    to_process = []
+    for p in all_playlists:
+        if p["title"] in cfg["organise"]:
+            to_process.append(p)
+
+    # Step 1:
+    # Write the "all" playlist to a file so we never lose track.
+
+    # Step 2:
+    # Delete the "TODO" playlist.
+    # Then recreate it with all the songs that are in the "all" playlist, but in no other playlist.
