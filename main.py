c266f7e8dce0e80aaf3deb8c18d734a50df0ac85
Conor O'Brien <obriencjj@gmail.com>
2025-12-14 06:25:55 +0000

    Write the state of the 'all' playlist

--- main.py
+++ main.py
@@ -33,13 +33,25 @@ if __name__ == "__main__":
     # Loop 2: filter the relevant playlists.
     # Yes, this could be 1 loop. This also isn't NASA.
     # We're not solving for performance.
-    to_process = []
+    to_process = dict()
     for p in all_playlists:
         if p["title"] in cfg["organise"]:
-            to_process.append(p)
+            to_process[p["title"]] = p
 
-    # Step 1:
     # Write the "all" playlist to a file so we never lose track.
+    all_titles_and_artists = []
+    for track in ytmusic.get_playlist(
+        to_process["all"]["playlistId"],
+        limit=int(1e5),
+    )["tracks"]:
+        title = track["title"]
+        artists = [a["name"] for a in track["artists"]]
+        all_titles_and_artists.append(f"{title} :: {', '.join(artists)}")
+
+    with open("out/all.txt", "w") as f:
+        f.write("\n".join(all_titles_and_artists))
+
+    # Write the other playlists to while we're at it.
 
     # Step 2:
     # Delete the "TODO" playlist.
